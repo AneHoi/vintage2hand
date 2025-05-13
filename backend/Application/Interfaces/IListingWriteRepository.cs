f259b325ed947f5b4a1527c091cb71f7e46e29b0
Anehoimark <anehoimark@gmail.com>
2025-05-13 12:41:00 +0200

    Adding comment about unit of work to interface

--- backend/Application/Interfaces/IListingWriteRepository.cs
+++ backend/Application/Interfaces/IListingWriteRepository.cs
@@ -2,5 +2,13 @@ namespace domain.Interfaces;
 
 public interface IListingWriteRepository
 {
+    /*  Update data in repository
+     *  Unit of work research – in repositories.
+     * Transaction starts
+     * Write to SQL database
+     * Write to NoSQL Repository
+     * Transaction finish
+     */
+    Listing AddItemToStorrage(Listing listing);
     
 }
